05f6c0095b584e8b182ace45bd642ae76363146f
karuu-01 <brian.muchori@student.moringaschool.com>
2021-09-05 16:45:17 +0300

    add toast and a new class activity

--- app/src/main/java/com/moringaschool/coffeeplace/MainActivity.java
+++ app/src/main/java/com/moringaschool/coffeeplace/MainActivity.java
@@ -3,12 +3,24 @@ package com.moringaschool.coffeeplace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindPlacesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindPlacesButton = (Button) findViewById(R.id.FindPlacesButton);
+        mFindPlacesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //added a toast
+                Toast.makeText(MainActivity.this, "Travel Is My Therapy", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
