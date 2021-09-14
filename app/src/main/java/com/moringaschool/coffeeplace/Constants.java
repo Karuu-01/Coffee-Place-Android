cbaae0622a6b8a579930540f39d605b57c2235fa
karuu-01 <brian.muchori@student.moringaschool.com>
2021-09-14 11:00:30 +0300

    Add @Parcel dependency in classes

--- app/src/main/java/com/moringaschool/coffeeplace/Constants.java
+++ app/src/main/java/com/moringaschool/coffeeplace/Constants.java
@@ -2,5 +2,6 @@ package com.moringaschool.coffeeplace;
 
 public class Constants {
     public static final String TRAVEL_BASE_URL = "https://api.yelp.com/v3/";
+    String newUrlString = TRAVEL_BASE_URL.replace("http", "https");
     public static final String TRAVEL_API_KEY = BuildConfig.TRAVEL_API_KEY;
 }
