c7e466a6a8d1b796778a97e1fef1d5f248b3b70d
karuu-01 <brian.muchori@student.moringaschool.com>
2021-09-18 09:26:48 +0300

    add project

--- app/src/main/java/com/moringaschool/coffeeplace/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.moringaschool.coffeeplace;
-
-public class Constants {
-    public static final String TRAVEL_BASE_URL = "http://api.opentripmap.com/0.1/en/";
-    String newUrlString = TRAVEL_BASE_URL.replace("http", "https");
-    public static final String TRAVEL_API_KEY = BuildConfig.TRAVEL_API_KEY;
-    public static final String KEY = BuildConfig.KEY;
-}
