9225aae28d94a6ed01ef4a42e1b93eae0d02954a
karuu-01 <brian.muchori@student.moringaschool.com>
2021-09-21 01:58:40 +0300

    implement firebase node

--- app/src/main/java/com/moringaschool/newsapp/ui/HomeFragment.java
+++ app/src/main/java/com/moringaschool/newsapp/ui/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +19,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.moringaschool.newsapp.Constants;
 import com.moringaschool.newsapp.R;
 import com.moringaschool.newsapp.adapters.Adapter;
 import com.moringaschool.newsapp.models.ArticleNews;
@@ -25,12 +28,14 @@ import com.moringaschool.newsapp.network.NewsClientApi;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
 
     String api = "e4ec187190a44ade83b0e610b5e9aa95";
@@ -39,6 +44,8 @@ public class HomeFragment extends Fragment {
     String country = "us";
     private RecyclerView recyclerViewHome;
     private String category = "business";
+    @BindView(R.id.fragButton)
+    Button mFragButton;
 
 
 
@@ -46,6 +53,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, null);
+        ButterKnife.bind(this, view);
 
         recyclerViewHome = view.findViewById(R.id.recyclerViewHome);
         articleNewsArrayList = new ArrayList<>();
@@ -77,5 +85,17 @@ public class HomeFragment extends Fragment {
 
                     }
                 });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mFragButton) {
+            DatabaseReference newsRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(Constants.FIREBASE_CHILD_NEWS);
+            newsRef.push().setValue(recyclerViewHome);
+            Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
